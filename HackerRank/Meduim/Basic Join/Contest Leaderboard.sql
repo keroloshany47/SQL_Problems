9deee098c3015ba2e62a9576739fa7c15b6a6362
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-11 08:01:13 +0300

    Update Contest Leaderboard.sql

--- HackerRank/Meduim/Basic Join/Contest Leaderboard.sql
+++ HackerRank/Meduim/Basic Join/Contest Leaderboard.sql
@@ -1,13 +1,13 @@
 SELECT h.hacker_id,
        h.name,
-       SUM(bs.max_score) AS total_score
-FROM Hackers h
+       SUM(best_score.max_score) AS total_score
+FROM Hackers AS h
 JOIN (
     SELECT hacker_id, challenge_id, MAX(score) AS max_score
     FROM Submissions
     GROUP BY hacker_id, challenge_id
-) AS best_scores
-  ON h.hacker_id = best_scores.hacker_id
+) AS best_score 
+  ON h.hacker_id = best_score.hacker_id
 GROUP BY h.hacker_id, h.name
-HAVING SUM(best_scores.max_score) > 0
+HAVING SUM(best_score.max_score) > 0
 ORDER BY total_score DESC, h.hacker_id ASC;
