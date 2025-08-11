8250ab35ef64bd2440c7c63bfec5bf92df992b9c
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-11 07:51:19 +0300

    Update Challenges.sql

--- HackerRank/Meduim/Basic Join/Challenges.sql
+++ HackerRank/Meduim/Basic Join/Challenges.sql
@@ -1,19 +1,21 @@
 WITH counts AS (
     SELECT h.hacker_id, h.name, COUNT(c.challenge_id) AS total_challenges
     FROM Hackers h
-    JOIN Challenges c ON h.hacker_id = c.hacker_id
+    JOIN Challenges c 
+      ON h.hacker_id = c.hacker_id
     GROUP BY h.hacker_id, h.name
 ),
 max_count AS (
     SELECT MAX(total_challenges) AS max_ch FROM counts
 )
-SELECT *
-FROM counts
-WHERE total_challenges = (SELECT max_ch FROM max_count)
-   OR total_challenges NOT IN (
+SELECT c.*
+FROM counts AS  c
+JOIN max_count AS mc
+  ON c.total_challenges = mc.max_ch
+   OR c.total_challenges NOT IN (
         SELECT total_challenges
         FROM counts
         GROUP BY total_challenges
         HAVING COUNT(*) > 1
    )
-ORDER BY total_challenges DESC, hacker_id;
+ORDER BY c.total_challenges DESC, c.hacker_id;
