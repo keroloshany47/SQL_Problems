f71a6184b859cf3dac60fbc27a0f768d14fc686b
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-13 03:05:56 +0300

    Update 1070. Product Sales Analysis III.sql

--- LeetCode/Meduim/Sorting and Grouping/1070. Product Sales Analysis III.sql
+++ LeetCode/Meduim/Sorting and Grouping/1070. Product Sales Analysis III.sql
@@ -1,1 +1,13 @@
-
+SELECT 
+    s.product_id,
+    p.first_year,
+    s.quantity,
+    s.price
+FROM Sales AS s
+JOIN (
+    SELECT product_id, MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+)AS p 
+    ON s.product_id = p.product_id 
+    AND s.year = p.first_year;
