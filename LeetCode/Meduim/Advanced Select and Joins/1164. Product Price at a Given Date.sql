993e0d0fa2c020477d885f9146584c8667efa5b9
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-15 01:56:55 +0300

    Update 1164. Product Price at a Given Date.sql

--- LeetCode/Meduim/Advanced Select and Joins/1164. Product Price at a Given Date.sql
+++ LeetCode/Meduim/Advanced Select and Joins/1164. Product Price at a Given Date.sql
@@ -1,1 +1,16 @@
+SELECT 
+    p.product_id,
+    IFNULL(pr.new_price, 10) AS price
+FROM (
+    SELECT DISTINCT product_id
+    FROM Products
+) AS p
+LEFT JOIN Products AS pr
+ON p.product_id = pr.product_id
+   AND pr.change_date = (
+       SELECT MAX(change_date)
+       FROM Products
+       WHERE product_id = p.product_id
+         AND change_date <= '2019-08-16'
+   );
 
