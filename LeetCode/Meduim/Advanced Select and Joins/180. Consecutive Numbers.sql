8662fa2001706b534cf628128b65b4a6dcbc3e60
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-15 01:56:41 +0300

    Update 180. Consecutive Numbers.sql

--- LeetCode/Meduim/Advanced Select and Joins/180. Consecutive Numbers.sql
+++ LeetCode/Meduim/Advanced Select and Joins/180. Consecutive Numbers.sql
@@ -1,1 +1,8 @@
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs AS l1
+JOIN Logs  AS l2 
+ON l2.id = l1.id + 1
+JOIN Logs AS l3 
+ON l3.id = l1.id + 2
+WHERE l1.num = l2.num AND l1.num = l3.num;
 
