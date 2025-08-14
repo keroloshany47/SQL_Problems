ff0c1b458967ed446bb8ce284d3631318f21070b
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-15 01:51:57 +0300

    Update 185. Department Top Three Salaries.sql

--- LeetCode/Hard/185. Department Top Three Salaries.sql
+++ LeetCode/Hard/185. Department Top Three Salaries.sql
@@ -1,1 +1,14 @@
-
+SELECT 
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM Employee AS  E
+JOIN Department AS D 
+ON E.departmentId = D.id
+WHERE (
+    SELECT COUNT(DISTINCT E2.salary)
+    FROM Employee AS E2
+    WHERE E2.departmentId = E.departmentId 
+    AND E2.salary > E.salary
+) < 3
+ORDER BY D.name, E.salary DESC;
