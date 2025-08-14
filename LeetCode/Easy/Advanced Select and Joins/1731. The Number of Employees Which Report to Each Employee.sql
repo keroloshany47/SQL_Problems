69264f27edaaa5fba6a425013166592611ddd5a6
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-14 03:23:16 +0300

    Update 1731. The Number of Employees Which Report to Each Employee.sql

--- LeetCode/Easy/Advanced Select and Joins/1731. The Number of Employees Which Report to Each Employee.sql
+++ LeetCode/Easy/Advanced Select and Joins/1731. The Number of Employees Which Report to Each Employee.sql
@@ -1,1 +1,12 @@
+SELECT
+  E1.employee_id,
+  E1.name,
+  COUNT(E2.employee_id) AS reports_count,
+  ROUND(AVG(E2.age)) AS average_age
+FROM Employees AS E1
+LEFT JOIN Employees AS E2
+  ON E2.reports_to = E1.employee_id
+GROUP BY E1.employee_id, E1.name
+HAVING COUNT(E2.employee_id) > 0
+ORDER BY E1.employee_id;
 
