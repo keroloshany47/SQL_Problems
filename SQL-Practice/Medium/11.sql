6d4707fbb37a709058259e9814699654ed469139
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-19 18:20:27 +0300

    Create 11.sql

---
+++ SQL-Practice/Medium/11.sql
@@ -0,0 +1,9 @@
+--Show all allergies ordered by popularity. Remove NULL values from query.
+SELECT
+  allergies,
+  COUNT(*) AS total_diagnosis
+FROM patients
+WHERE
+  allergies IS NOT NULL
+GROUP BY allergies
+ORDER BY total_diagnosis DESC
