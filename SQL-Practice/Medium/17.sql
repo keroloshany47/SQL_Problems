ac4628c8b57efcf7b58cd79817bdee5fea262a73
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-20 00:38:57 +0300

    Create 17.sql

---
+++ SQL-Practice/Medium/17.sql
@@ -0,0 +1,7 @@
+--Show all columns for patient_id 542's most recent admission_date
+SELECT *
+FROM admissions
+WHERE patient_id = 542
+GROUP BY patient_id
+HAVING
+  admission_date = MAX(admission_date);
