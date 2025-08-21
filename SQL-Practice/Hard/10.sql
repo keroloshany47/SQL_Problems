3d196597345293c587f8c4f8e705e166d8d9e2ae
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-22 01:44:48 +0300

    Update 10.sql

--- SQL-Practice/Hard/10.sql
+++ SQL-Practice/Hard/10.sql
@@ -1,1 +1,11 @@
+--Sort the province names in ascending order in such a way that the province 'Ontario' is always on top.
+
+SELECT province_name
+FROM province_names
+ORDER BY 
+    CASE 
+        WHEN province_name = 'Ontario' THEN 0 
+        ELSE 1 
+    END,
+    province_name ASC;
 
