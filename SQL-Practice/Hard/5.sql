a0968dcf4c6ca015d5d0b7dc6d1c4e153acc8fa5
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-21 18:44:37 +0300

    Update 5.sql

--- SQL-Practice/Hard/5.sql
+++ SQL-Practice/Hard/5.sql
@@ -1,1 +1,24 @@
-
+--Each admission costs $50 for patients without insurance, 
+--and $10 for patients with insurance. All patients with an even patient_id have insurance.
+-- Give each patient a 'Yes' if they have insurance, and a 'No' if they don't have insurance. 
+-- Add up the admission_total cost for each has_insurance group.
+SELECT 
+CASE WHEN patient_id % 2 = 0 Then 
+    'Yes'
+ELSE 
+    'No' 
+END as has_insurance,
+SUM(CASE WHEN patient_id % 2 = 0 Then 
+    10
+ELSE 
+    50 
+END) as cost_after_insurance
+FROM admissions 
+GROUP BY has_insurance;
+-----------------------------------------------------------------------
+select 'No' as has_insurance, count(*) * 50 as cost
+from admissions where patient_id % 2 = 1 group by has_insurance
+union
+select 'Yes' as has_insurance, count(*) * 10 as cost
+from admissions where patient_id % 2 = 0 group by has_insurance
+-----------------------------------------------------------------------
