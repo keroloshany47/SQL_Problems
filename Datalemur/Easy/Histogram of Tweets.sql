f0314d4ec4da7ecc01272aa44aa98b3259baf240
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-23 00:43:00 +0300

    Update Histogram of Tweets.sql

--- Datalemur/Easy/Histogram of Tweets.sql
+++ Datalemur/Easy/Histogram of Tweets.sql
@@ -1,14 +1,16 @@
 SELECT
     tweet_count AS tweet_bucket,
     COUNT(*) AS users_num
+    
 FROM (
-    SELECT
-        user_id,
-        COUNT(*) AS tweet_count
-    FROM tweets
-    WHERE tweet_date >= '2022-01-01'
-      AND tweet_date < '2023-01-01'
-    GROUP BY user_id
-) AS user_tweet_counts
+            SELECT
+            user_id,
+            COUNT(*) AS tweet_count
+            FROM tweets
+            WHERE tweet_date >= '2022-01-01'
+            AND tweet_date < '2023-01-01'
+            GROUP BY user_id
+    ) AS user_tweet_counts
+    
 GROUP BY tweet_count
 ORDER BY tweet_bucket;
