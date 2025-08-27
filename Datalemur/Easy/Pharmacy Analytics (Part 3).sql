bc8bd7698c977fdaf9fcfe4255c0a718ba86259f
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-27 03:38:24 +0300

    Update Pharmacy Analytics (Part 3).sql

--- Datalemur/Easy/Pharmacy Analytics (Part 3).sql
+++ Datalemur/Easy/Pharmacy Analytics (Part 3).sql
@@ -1,1 +1,5 @@
-
+SELECT manufacturer,
+       CONCAT("$", ROUND(SUM(total_sales)/1000000, 0), " million") AS sale
+FROM pharmacy_sales
+GROUP BY manufacturer
+ORDER BY SUM(total_sales) DESC, manufacturer;
