ca2fe17b9341988e964202a9979a57b52a3df3d8
kerolos hani <153551384+keroloshany47@users.noreply.github.com>
2025-08-27 03:33:27 +0300

    Update Pharmacy Analytics (Part 2).sql

--- Datalemur/Easy/Pharmacy Analytics (Part 2).sql
+++ Datalemur/Easy/Pharmacy Analytics (Part 2).sql
@@ -1,1 +1,8 @@
-
+SELECT
+  manufacturer,
+  COUNT(drug) AS drug_count,
+  ABS(SUM(total_sales - cogs)) AS total_loss
+FROM pharmacy_sales
+WHERE total_sales - cogs <= 0
+GROUP BY manufacturer
+ORDER BY total_loss DESC;
