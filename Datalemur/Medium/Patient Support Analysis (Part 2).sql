f7746d3f8d573014a8c0d053a9eb59ec7e608d29
kerolos hani <keroloshani474@gmail.com>
2025-09-12 02:41:45 +0300

    Create Patient Support Analysis (Part 2).sql

---
+++ Datalemur/Medium/Patient Support Analysis (Part 2).sql
@@ -0,0 +1,7 @@
+SELECT 
+    ROUND(
+        100.0 * SUM(CASE WHEN call_category ='n/a' OR call_category IS NULL THEN 1 ELSE 0 END) 
+        / COUNT(*), 
+        1
+    ) AS uncategorised_call_pct 
+FROM callers;
