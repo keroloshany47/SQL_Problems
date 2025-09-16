626f0452c4303dd9e11fa9e723fad5e8e73136f1
kerolos hani <keroloshani474@gmail.com>
2025-09-16 09:03:12 +0300

    Update Active User Retention.sql

--- Datalemur/Hard/Active User Retention.sql
+++ Datalemur/Hard/Active User Retention.sql
@@ -1,1 +1,15 @@
+SELECT 
+    7 AS mth,
+    COUNT(DISTINCT july.user_id) AS monthly_active_users
+FROM (
+    SELECT DISTINCT user_id
+    FROM user_actions
+    WHERE event_date BETWEEN '2022-06-01 00:00:00' AND '2022-06-30 23:59:59'
+) AS june
+JOIN (
+    SELECT DISTINCT user_id
+    FROM user_actions  
+    WHERE event_date BETWEEN '2022-07-01 00:00:00' AND '2022-07-31 23:59:59'
+) AS july
+ON july.user_id = june.user_id;
 
