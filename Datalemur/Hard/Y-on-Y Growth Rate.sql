0d423d79a70e01ff3e08323b0ed8a78c632dfa3e
kerolos hani <keroloshani474@gmail.com>
2025-09-17 10:41:54 +0300

    Update Y-on-Y Growth Rate.sql

--- Datalemur/Hard/Y-on-Y Growth Rate.sql
+++ Datalemur/Hard/Y-on-Y Growth Rate.sql
@@ -21,3 +21,27 @@ SELECT
     ) AS yoy_rate
 FROM yearly_spend
 ORDER BY product_id, year;
+
+--------------------------------------------------------------------------------------
+SELECT
+    year,
+    product_id,
+    curr_year_spend,
+    LAG(curr_year_spend) OVER (
+        PARTITION BY product_id
+        ORDER BY year
+    ) AS prev_year_spend,
+    ROUND(
+        100.0 * (curr_year_spend - LAG(curr_year_spend) OVER (PARTITION BY product_id ORDER BY year))
+              / LAG(curr_year_spend) OVER (PARTITION BY product_id ORDER BY year),
+        2
+    ) AS yoy_rate
+FROM  (
+    SELECT
+        EXTRACT(YEAR FROM transaction_date) AS year,
+        product_id,
+        SUM(spend) AS curr_year_spend
+    FROM user_transactions
+    GROUP BY EXTRACT(YEAR FROM transaction_date), product_id
+) As yearly_spend 
+ORDER BY product_id, year;
